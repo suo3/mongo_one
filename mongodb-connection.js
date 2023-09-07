74e5a2692812048cb3fc194b9571d57b5adaff2c
suo3 <obiaproduction@gmail.com>
2023-09-06 20:07:07 -0400

    connect to mongodb

--- mongodb-connection.js
+++ mongodb-connection.js
@@ -1,21 +1,41 @@
-const MongoDBClient = require("mongodb").MongoClient;
+const { MongoClient } = require("mongodb");
 
 //connection URL string
-const url = "mongodb://localhost:27017";
+const url = "mongodb://0.0.0.0:27017/";
+dbname = "BPBOnlineBooksDB";
 
-//connecting to mongodb server using connect method
-MongoDBClient.connect(
-  url,
-  { useUnifiedTopology: true },
-  function (err, client) {
-    if (err) {
-      console.log("Some Error While Connecting to MongoDB Server" + err);
-    } else {
-      console.log(
-        "Connected Successfully to MongoDB Server using Node.js Driver for MongoDB"
-      );
-    }
-    //close the Server Connection
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = "BPBOnlineBooksDB";
+
+const dbConnection = async () => {
+  try {
+    // Use connect method to connect to the server
+    await client.connect();
+    console.log("Connected successfully to server");
+
+    const db = client.db(dbName);
+    const books = db.collection("BPBOnlineBooksCollection");
+    //Add a new book to the collection
+    const insertNewBook = await books.insertOne({
+      "Book-title": "Harry Potter",
+      "Book-author": "JK Rawling",
+      "Book-ISBN": "2234567890129",
+      "Book-pages": "500",
+      "Book-brief-description": "The wizard world"
+    });
+
+    const listBooks = await books
+      .find({ "Book-title": "Harry Potter" })
+      .toArray();
+    console.log(listBooks);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    //close client on finish/error
     client.close();
   }
-);
+};
+
+dbConnection().catch(console.error);
